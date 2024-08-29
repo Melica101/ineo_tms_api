0abb12495b6d9e8ab009b9e47b7eb5dc1174a984
Melica101 <amalicamelly@gmail.com>
2024-08-29 21:39:59 +0200

    index file changed

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json'); // Your database file
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 server.use(router);
-
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-server.use(cors(corsOptions))
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.use(middlewares);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
 });
