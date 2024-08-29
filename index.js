8f2af9beff80d15a06bba6025afc39093655d9c4
Melica101 <amalicamelly@gmail.com>
2024-08-29 21:36:43 +0200

    cors added

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+server.use(cors(corsOptions))
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
